5dbed3f719c4842d3f72a104c1497db5bebb6c65
Ric Mershon <rmersh@ricmershon.com>
2020-11-09 16:38:17 -0500

    Added Header component with logo and SHOP link functionality.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { Route, Switch } from 'react-router-dom';
-import HomePage from './pages/HomePage/HomePage';
-import ShopPage from './pages/ShopPage/ShopPage'
+
 import './App.css';
+
+import HomePage from './pages/HomePage/HomePage';
+import ShopPage from './pages/ShopPage/ShopPage';
+import Header from './components/Header/Header';
 
 function App() {
     return (
         <div>
+            <Header />
             <Switch>
                 <Route exact path ='/' component={ HomePage } />
                 <Route path ='/shop' component={ ShopPage } />
